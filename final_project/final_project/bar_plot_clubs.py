b6db702d1fb8f2cef2d92cad3f67b73f82ae2322
Diogo Fontes <84334953+DiogoFontes01@users.noreply.github.com>
2025-12-14 22:39:14 +0000

    Add files via upload

---
+++ final_project/final_project/bar_plot_clubs.py
@@ -0,0 +1,101 @@
+"""
+This module contains code used to create plot with clubs 
+that are increasing value of football players most . 
+"""
+import pandas as pd
+import plotly.graph_objects as go
+
+
+def create_plot_club_increasing_value(position: str):
+    # colors = {"Attackers": "#BB4430", "Defenders": "#53917E", "Midfielders": "#7EBDC2"}
+
+    colors = {
+        "Defenders": "#A9D78E",
+        "Midfielders": "#9DC3C2",
+        "Attackers": "#6B97C3",
+    }
+
+
+    df = pd.read_csv("data/club_value_increase.csv")
+    df = df[df["position"] == position[:-1]]
+    df = df.sort_values(by="value_increase", ascending=False)
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Bar(
+            x=df["value_increase"],
+            y=list(range(10, 0, -1)),
+            name="att",
+            marker=dict(
+                color=colors[position],
+                line=dict(color=colors[position], width=1),
+            ),
+            orientation="h",
+        )
+    )
+
+    fig.update_layout(
+        title=dict(
+            text="If you have a chance, pick one of this clubs <br><sup>Not all clubs are equally good at increasing value of players in different positions</sup>",
+            font_size=30,
+            font_family="Arial",
+            x=0.25,
+        ),
+        yaxis=dict(
+            showgrid=False,
+            showline=False,
+            showticklabels=False,
+            domain=[0, 1],
+        ),
+        xaxis=dict(
+            zeroline=False,
+            showline=False,
+            showticklabels=False,
+            showgrid=True,
+            domain=[0.2, 0.8],
+        ),
+        showlegend=False,
+        plot_bgcolor="white",
+    )
+
+    # add annotations
+    for i in range(0, 10):
+        for j, df in enumerate([df]):
+            idx = 10 - i - 1
+            fig.add_annotation(
+                dict(
+                    xref="x",
+                    yref="y",
+                    x=0,
+                    y=i + 1,
+                    text=df["club_name"].iloc[idx][:20],
+                    font=dict(family="Arial", size=12, color="black"),
+                    showarrow=False,
+                    align="right",
+                    xanchor="right",
+                )
+            )
+            # add value to each bar
+            fig.add_annotation(
+                dict(
+                    xref="x",
+                    yref="y",
+                    x=df["value_increase"].iloc[idx],
+                    y=i + 1,
+                    text=str(round(df["value_increase"].iloc[idx], 2)),
+                    font=dict(family="Arial", size=12, color="black"),
+                    showarrow=False,
+                    align="left",
+                    xanchor="left",
+                )
+            )
+
+    return fig
+
+
+if __name__ == "__main__":
+    fig = create_plot_club_increasing_value("Attackers")
+    fig.show()
+    # save as html
+    fig.write_html("images/attacker.html")
---
+++ final_project/final_project/dashboard.py
@@ -0,0 +1,220 @@
+import pandas as pd
+import plotly.graph_objects as go
+from dash import Dash, dcc, html, Input, Output, State, callback
+from plot_value_diff_by_position import create_plot_value_per_position
+from plot_number_of_players_per_position import number_of_players_per_position_plot
+from bar_plot_clubs import create_plot_club_increasing_value
+from plot_managers import plot_best_managers
+from plot_leagues import plot_leagues
+import plotly.express as px
+import dash_bootstrap_components as dbc
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = Dash(
+    __name__,
+    suppress_callback_exceptions=True,
+    external_stylesheets=[
+        dbc.themes.BOOTSTRAP,
+        "https://codepen.io/chriddyp/pen/bWLwgP.css",
+    ],
+    prevent_initial_callbacks="initial_duplicate",
+)
+
+app.layout = html.Div(
+    [
+        html.H1("Footballers guide to becoming MVP", style={"textAlign": "center"}),
+        dcc.Tabs(
+            id="tabs-example-graph",
+            value="tab-1-example-graph",
+            children=[
+                dcc.Tab(
+                    label="Which position",
+                    value="tab-1-example-graph",
+                ),
+                dcc.Tab(label="Trending positions", value="tab-2-example-graph"),
+                dcc.Tab(label="Which league", value="tab-5-example-graph"),
+                dcc.Tab(label="Which club", value="tab-3-example-graph"),
+                dcc.Tab(label="Which manager", value="tab-4-example-graph"),
+            ],
+        ),
+        dcc.Store(id="graph-storage"),
+        dcc.Store(id="graph-tab3-storage"),
+        html.Div(id="tabs-content-example-graph"),
+        dbc.Row(
+            [
+                dbc.Col(md=8),
+                dbc.Col(
+                    html.H6(
+                        "Data source: Transfermarkt",
+                        style={"textAlign": "right", "justify": "right"},
+                    ),
+                    md=4,
+                ),
+            ],
+            align="center",
+        ),
+    ]
+)
+
+
+@callback(
+    [Output("tabs-content-example-graph", "children"), Output("graph-storage", "data")],
+    Input("tabs-example-graph", "value"),
+    [State("graph-storage", "data")],
+)
+def render_content(tab, stored_data):
+    if stored_data is None:
+        stored_data = {}
+
+    if tab == "tab-1-example-graph":
+        fig = stored_data.get("fig1") or create_plot_value_per_position()
+        stored_data["fig1"] = fig
+        return (
+            html.Div(
+                [dcc.Graph(id="positions_field", figure=fig)],
+                style=dict(display="flex", justifyContent="center", padding=20),
+            ),
+            stored_data,
+        )
+
+    elif tab == "tab-2-example-graph":
+        fig = stored_data.get("fig2") or number_of_players_per_position_plot()
+        stored_data["fig2"] = fig
+        return (
+            html.Div(
+                [dcc.Graph(id="graph-2-tabs-dcc", figure=fig)],
+                style=dict(display="flex", justifyContent="center", padding=20),
+            ),
+            stored_data,
+        )
+
+    elif tab == "tab-3-example-graph":
+        return (
+            html.Div(
+                [
+                    dcc.Graph(id="graph-3-tabs-dcc"),
+                    dcc.RadioItems(
+                        [
+                            {
+                                "label": html.Div(
+                                    ["Defenders"],
+                                    style={
+                                        "color": "#A9D78E",
+                                        "font-size": 20,
+                                        "padding": 0,
+                                        "margin-bottom": 0,
+                                    },
+                                ),
+                                "value": "Defenders",
+                            },
+                            {
+                                "label": html.Div(
+                                    ["Midfielders"],
+                                    style={
+                                        "color": "#9DC3C2",
+                                        "font-size": 20,
+                                        "padding-left": 7,
+                                        "padding-right": 7,
+                                        "margin-bottom": 0,
+                                    },
+                                ),
+                                "value": "Midfielders",
+                            },
+                            {
+                                "label": html.Div(
+                                    ["Attackers"],
+                                    style={
+                                        "color": "#6B97C3",
+                                        "font-size": 20,
+                                        "padding": 0,
+                                        "margin-bottom": 0,
+                                    },
+                                ),
+                                "value": "Attackers",
+                            },
+                        ],
+                        value="Defenders",
+                        id="radio-items-positions",
+                        inline=True,
+                        style=dict(display="flex", justifyContent="center", padding=20),
+                        # make it look like a rectangle
+                        labelStyle=dict(
+                            display="flex",
+                            justifyContent="center",
+                            alignItems="center",
+                            width=150,
+                            height=50,
+                            border="2px solid",
+                            borderRadius=10,
+                            margin=10,
+                            backgroundColor="#F4F4F4",
+                        ),
+                        inputStyle=dict(display="none"),
+                        inputClassName="radio-items-positions",
+                    ),
+                ]
+            ),
+            stored_data,
+        )
+
+    elif tab == "tab-4-example-graph":
+        fig = stored_data.get("fig3") or plot_best_managers()
+        stored_data["fig3"] = fig
+        return (
+            html.Div(
+                [
+                    dcc.Graph(
+                        id="graph-4-tabs-dcc",
+                        figure=fig,
+                    ),
+                ]
+            ),
+            stored_data,
+        )
+
+    elif tab == "tab-5-example-graph":
+        fig = stored_data.get("fig4") or plot_leagues()
+        stored_data["fig4"] = fig
+        return (
+            html.Div(
+                [
+                    dcc.Graph(
+                        id="graph-4-tabs-dcc",
+                        figure=fig,
+                    ),
+                ],
+                style=dict(display="flex", justifyContent="center", padding=20),
+            ),
+            stored_data,
+        )
+
+    return html.Div(), stored_data
+
+
+@callback(
+    [
+        Output("graph-3-tabs-dcc", "figure"),
+        Output("graph-tab3-storage", "data"),
+    ],
+    Input(component_id="radio-items-positions", component_property="value"),
+    State("graph-tab3-storage", "data"),
+)
+def render_third_tab(position, stored_data):
+    if stored_data is None:
+        stored_data = {}
+
+    fig = stored_data.get(f"fig_{position}") or create_plot_club_increasing_value(
+        position
+    )
+
+    stored_data.update({f"fig_{position}": fig})
+
+    return (
+        fig,
+        stored_data,
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
